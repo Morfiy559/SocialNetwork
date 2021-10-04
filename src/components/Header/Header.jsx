1752dd9a51d0f134e0555fc4ee709112385d81d8
Max <maks-berd@mail.ru>
2021-10-05 00:48:33 +0300

    61 auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import s from './Header.module.css';
-const Header = () => {
+import {NavLink} from "react-router-dom";
+const Header = (props) => {
     return(
         <header className={s.header}>
             <img src='https://w7.pngwing.com/pngs/1020/508/png-transparent-google-chrome-computer-icons-web-browser-desktop-chrome.png'/>
+
+        <div className={s.loginBlock}>
+            {
+                props.isAuth?
+                    props.login
+                    : <NavLink to='/login'>Login</NavLink>
+            }
+        </div>
         </header>
     )
 }
--- src/components/Profile/MyPost/MyPosts.jsx
+++ src/components/Profile/MyPost/MyPosts.jsx
@@ -5,7 +5,6 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-    let newPostElement = React.createRef();
 
     let onAddPost = () => {
         props.addPost();
