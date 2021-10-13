a19d0f36fb6a72fc675cd0c6b97b81a7027c7402
Max <maks-berd@mail.ru>
2021-10-14 00:08:17 +0300

    useState hook

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,7 +3,8 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/preloader";
 import palecUp from '../../../assets/images/Thumbsup.png';
 import palecDown from '../../../assets/images/Thumbsdown.png';
-import Status from './Status';
+import Status from './ProfileStatus';
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -17,7 +18,7 @@ const ProfileInfo = (props) => {
             {/*        src='https://lh3.googleusercontent.com/q_oxbEd3XEpegpDVZyYYhtMZ_VIxrwSHguCZiKme4K11m_QMlh41mtzISbUpwi0AZP8=h500'/>*/}
             {/*</div>*/}
             <img src={props.profile.photos.large} alt="photo"/>
-            <Status status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             <div>{props.profile.aboutMe}</div>
             <div>{props.profile.fullName}</div>
             <div>{props.profile.lookingForAJob? <img src={palecUp} alt=""/>: <img src={palecDown} alt=""/>}</div>
